5c400370c57572e8b27145a366bbe78baaacdd8d
kennysalam <kennys.salam@gmail.com>
2020-01-10 00:16:50 +0700

    weatherapi server done

--- controllers/weather.js
+++ controllers/weather.js
@@ -1,5 +1,21 @@
-class WeatherController {
+const axios = require('axios')
+const url = 'https://api.openweathermap.org/data/2.5/weather'
 
+class WeatherController {
+    static getWeather(req,res,next){
+        let lat = req.params.lat
+        let lon = req.params.lon
+        axios.get(`${url}/?lat=${lat}&lon=${lon}&APPID=${process.env.WEATHER_API_KEY}&units=metric`)
+        .then(({ data })=>{
+            let toSend = {}
+            toSend.main = data.weather[0].main
+            toSend.temp = `${data.main.temp} C`
+            toSend.img_url = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            res.status(200).json(toSend)
+        })
+        .catch(next)
+    }
 }
+
 
 module.exports = WeatherController
